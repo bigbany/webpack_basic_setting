0b63c2f3542e6a43396bac3159dab601d9834e08
bigbany <bigbay9728@naver.com>
2021-09-21 13:30:58 +0900

    'version1'

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>{{ count }}</h1>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      count: 2
+    }
+  },
+  beforeCreate(){
+    console.log('before created!',this.count)
+
+  },
+  created(){
+    console.log('created!',this.count)
+  },
+   beforeMount(){
+    console.log(' before mounted!')
+    console.log(
+      document.querySelector('h1')
+    )
+    
+  },
+  mounted(){
+    console.log('mounted!')
+    console.log(
+      document.querySelector('h1')
+    )
+  }
+}
+</script>
+
+
+<style >
+
+</style>
---
+++ src/components/Fruit.vue
@@ -0,0 +1,22 @@
+<template>
+  <li>{{ name }}5!</li>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: ''
+    }
+  }
+}
+
+</script>
+
+
+<style scoped lang="scss">
+h1 {
+  color: red !important;
+}
+</style>
